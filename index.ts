01b9164ad1e372d93e94ebb84fc1f22fd89ad67b
Shuruder <gabriel_brandini@hotmail.com>
2022-07-25 15:19:53 -0300

    Uso do Type e da Interface

--- index.ts
+++ index.ts
@@ -12,19 +12,42 @@ function soma(a: number, b: number) {
 
 // interfaces
 
+/*
 interface IAnimal {
     nome: string;
     tipo: 'terrestre' | 'aquatico';
     executarRugido(alturaEmDecibeis: Number): void;
 }
+*/
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+    domestico: boolean
+}
 
 const animal: IAnimal = {  //  Ctrl + Space indica o que contem a interface
     nome: 'Elefante',
     tipo: 'terrestre',
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+    domestico: false
+    //executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
 }
 
 interface IFelino extends IAnimal {
     visaoNoturna: Boolean;
 }
 
+// Como Usar o Type
+
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type IDomestico = IFelino | ICanino;  // type define junções e Merges entre as classes
+
+const animal2: IDomestico = {
+    nome: 'cachorro',
+    tipo: "terrestre",
+    domestico: true,
+    porte: "medio",
+}
