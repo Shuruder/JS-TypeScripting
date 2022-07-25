1cf88c7574c18594d9b2e0865bbec6bfee56cce3
Shuruder <gabriel_brandini@hotmail.com>
2022-07-25 14:00:00 -0300

    Funções são decclaraveis alem da tipagem

--- index.ts
+++ index.ts
@@ -5,3 +5,26 @@ function soma(a: number, b: number) {
 }
 
 //soma('a','b');  // typescript já indica os erros de tipagem no código
+
+
+// types
+
+
+// interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+    executarRugido(alturaEmDecibeis: Number): void;
+}
+
+const animal: IAnimal = {  //  Ctrl + Space indica o que contem a interface
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: Boolean;
+}
+
