dc826c69723ed1b1c52e29bae001dc0c0a21e2a0
Shuruder <gabriel_brandini@hotmail.com>
2022-07-25 17:52:31 -0300

    readonly e private

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
-interface IUsuario {
-    id: string;
-    email: string;
-    cargo?: 'gerente' | 'coordenador' | 'supervisor' | 'funcionario';
+interface Cachorro {
+    nome: string;
+    idade: number;
+    parqueFavorito?: string;
+}
+
+type CachorroRO = {
+    readonly [K in keyof Cachorro]: Cachorro[K]; // podendo remover valores opcionais com -? | [k in keyof cachorro]-?
+}
+
+class MeuCachorro implements CachorroRO  {
+     nome; // private não funciona por conta do implements
+     idade;
+
+    constructor(nome, idade) {
+        this.nome = nome ;
+        this.idade = idade;
+    } 
 }
 
 
-
-function redirecione(usurario: IUsuario) {
-   if (usurario.cargo) {
-    //redirecionar(usuário.cargo)
-   }
-
-   // redirecionar para area de usuario
-}
+const cao = new MeuCachorro('Billy', 14); // e se eu quiser que seja privado ou só leitura?
